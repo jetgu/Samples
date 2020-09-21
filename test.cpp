7f0f3bdeea7e3d8883d7d6928f54958f4670752f
jetgu <macrogu@yahoo.com>
2020-09-21 10:41:34 +0800

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -2,4 +2,13 @@
 This is the function for calculating the sum of 2 numbers 
 params: num1, num2 return: sum 
 */
- func calculate(num1,num2)
+ void calculate(int num1,int num2)
+ {
+  int total=0;
+  char buff[128];
+  sprintf(buff,"%s","good");
+  //calculate 
+   if(num1>0)
+     total = num1+num2;
+   return;
+ }
